cc2c843119e60047fa120092e33b05573d1e4c07
binnev <robin.m.neville@gmail.com>
2025-05-17 11:55:48 +0200

    NixOS legion gen 39

--- nixos/modules/rust.nix
+++ nixos/modules/rust.nix
@@ -7,19 +7,7 @@
     rustup
     gcc
     cargo-watch
-    pkg-config
-    openssl.dev
-    devenv # more configuration than nix shells?
-    direnv # automatically activate nix shells when entering the dir
   ];
-
-  # These are required because by default rust's compiler looks for these in
-  # their usual spot in the FHS. On NixOS, libraries like OpenSSL are not in
-  # global paths (e.g., /usr/include, /usr/lib) -- they’re in Nix store paths.
-  environment.variables = {
-    OPENSSL_DIR = "${pkgs.openssl.dev}";
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-  };
 
   nix.settings.trusted-users = ["root" "robin"];
 }

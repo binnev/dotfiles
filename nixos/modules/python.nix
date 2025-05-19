7d1777c1dda302674a66c9fb3706e133a8f9f754
binnev <robin.m.neville@gmail.com>
2025-05-19 10:19:19 +0200

    NixOS legion gen 47

--- nixos/modules/python.nix
+++ nixos/modules/python.nix
@@ -9,4 +9,11 @@
     ruff
     pre-commit
   ];
+
+  # allow running dynamically linked binaries (ruff is one of these)
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs here, NOT in
+    # environment.systemPackages
+  ];
 }

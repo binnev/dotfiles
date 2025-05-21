b2395b631b078ee24195e9b146d32eee93771551
binnev <robin.m.neville@gmail.com>
2025-05-21 17:59:24 +0200

    NixOS legion gen 49

--- nixos/hosts/legion/configuration.nix
+++ nixos/hosts/legion/configuration.nix
@@ -18,6 +18,7 @@
     ../../modules/networking.nix
     ../../modules/locale.nix
     ../../modules/rust.nix
+    ../../modules/python.nix
     ../../modules/i3.nix
     inputs.home-manager.nixosModules.default
   ];

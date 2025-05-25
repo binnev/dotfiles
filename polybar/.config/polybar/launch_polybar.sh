594b20a55534f91915901afcbde49b44b1473a0a
binnev <robin.m.neville@gmail.com>
2025-05-25 11:03:25 +0200

    rename top bar; update launch polybar script

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,7 +1,7 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
-  done
-else
-  polybar --reload toph &
-fi
+# Terminate already running bars
+polybar-msg cmd quit
+
+# Spawn the bar(s) on every connected monitor
+for m in $(polybar -m | cut -d ":" -f1); do 
+  MONITOR=$m polybar --reload top &
+done

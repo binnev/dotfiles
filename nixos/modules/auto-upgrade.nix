0751645c4032dc85c7f888cbd1eb72f537ef7132
binnev <robin.m.neville@gmail.com>
2025-06-01 17:38:40 +0200

    NixOS legion gen 56
    Added auto-garbage collection

--- nixos/modules/auto-upgrade.nix
+++ nixos/modules/auto-upgrade.nix
@@ -7,6 +7,9 @@
   # see: https://nixos.wiki/wiki/Automatic_system_upgrades
   system.autoUpgrade = {
     enable = true;
+
+    # Specifies that the system should upgrade using your current flake (the
+    # same repo/config you're using now).
     flake = inputs.self.outPath;
     flags = [
       "--update-input"
@@ -15,5 +18,12 @@
     ];
     dates = "02:00";
     randomizedDelaySec = "45min";
+  };
+
+  # Garbage collect old generations
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
   };
 }

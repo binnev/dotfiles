fd215710fdf275c873a3e2d86a210704b0f76349
binnev <robin.m.neville@gmail.com>
2025-07-07 13:32:29 +0200

    NixOS legion gen 75

--- nixos/modules/plex.nix
+++ nixos/modules/plex.nix
@@ -5,32 +5,14 @@
 }: {
   # read this https://wiki.nixos.org/w/index.php?title=Deluge&mobileaction=toggle_view_desktop
   environment.systemPackages = with pkgs; [
-    plex
     deluge
   ];
 
-  networking.firewall.allowedTCPPorts = [32400];
-
-  users.groups.plex = {};
-
-  users.users.plex = {
-    isSystemUser = true;
-    description = "Plex Media Server user";
-    home = "/var/lib/plex";
-    group = "plex";
+  services.plex = {
+    enable = true;
+    openFirewall = true;
   };
-
-  systemd.services.plex = {
-    description = "Plex media server";
-    after = ["network.target"];
-    wantedBy = [];
-    serviceConfig = {
-      ExecStart = "${pkgs.plex}/bin/plexmediaserver";
-      Restart = "on-failure";
-      User = "plex";
-      Group = "plex";
-    };
-  };
+  systemd.services."plex".wantedBy = []; # disable autostart
 
   services.mullvad-vpn = {
     enable = true;

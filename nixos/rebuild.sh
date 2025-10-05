ca0493a7aed6730b8dc104417349da8c5c32d01f
binnev <robin.m.neville@gmail.com>
2025-10-05 13:05:01 +0200

    NixOS legion gen 95

--- nixos/rebuild.sh
+++ nixos/rebuild.sh
@@ -6,10 +6,14 @@ git add --all
 # format .nix files
 alejandra . --quiet
 git diff -U0 *.nix
-sudo echo "NixOS Rebuilding..."
+
+# This updates flake.lock with newer values for nixpkgs etc
+echo "Updating flake.lock..."
+sudo nix flake update
 
 # This does NOT update the flake.lock. It just rebuilds the system using the 
 # exact stuff in flake.lock. 
+echo "Rebuilding..."
 sudo nixos-rebuild switch --flake .#$HOSTNAME &>~/nixos-switch.log || (
   cat ~/nixos-switch.log | grep --color error && false)
 gen=$(nixos-rebuild list-generations --json | jq "map(.generation) | max")
